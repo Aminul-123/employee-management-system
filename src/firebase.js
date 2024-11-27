678f3b17a0ea9f4950be3652606e87341981528a
Aminul Ali <alomamin963@gmail.com>
2024-11-27 21:54:52 +0530

    firebase js file included from gitignore

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+ export const firebaseConfig = {
+    apiKey: import.meta.env.VITE_API_KEY ,
+    authDomain: "fir-employee-management-ccff0.firebaseapp.com",
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: "fir-employee-management-ccff0.firebasestorage.app",
+    messagingSenderId: "673275031734",
+    appId: import.meta.env.VITE_API_ID
+};
