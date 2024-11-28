4de638376819f828112ff605590865570fe127c1
Aminul Ali <alomamin963@gmail.com>
2024-11-28 18:23:32 +0530

    search component

--- src/components/Applayout.jsx
+++ src/components/Applayout.jsx
@@ -3,7 +3,14 @@ import { Outlet } from 'react-router-dom'
 
 function Applayout() {
   return (
+    <>
     <Outlet />
+    <footer>
+        <p>
+          &copy; Copyright &lt;&lt; Made by <span>Aminul Ali, Barasha Rajbongshi and Rahima choudhary </span> &gt;&gt;
+        </p>
+    </footer>
+    </>
   )
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,31 +1,43 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import { Link } from 'react-router-dom';
 import Card from './Card';
 import { useFirebaseContext } from '../context/FirebaseContext';
-
+import {MoonLoader} from 'react-spinners'
 function Home() {
-  const {data, err} = useFirebaseContext()
+  const {data} = useFirebaseContext()
+  const [name, setName] = useState('')
 // console.log(err)
+   const cssLoader = {
+    marginLeft : '9rem'
+   }
    
+  
       
   return (
     <div className="home">
       <div className='employee-heading'>
-        <h2>ALL EMPLOYEES</h2>
+        <h2>🎑ALL EMPLOYEES CARDS🎑</h2>
         <Link to={'/add-employee'} className='add-employee-link' >Add Employee</Link>
+        <form>
+        <input type="text" className='search' placeholder='search Employee '
+       onChange={(e) => setName(e.target.value)}
+       />
+        </form>
       </div>
       <div className='all-cards'>
 
         {
           data.length === 0 ? (
             <>
-            <p className="loading">Loading.....</p>
+            <MoonLoader color='brown' cssOverride={cssLoader} />
             </>
           ) : (
             <>
             
         {
-         data && data.map((elem) => {
+         data && data.filter((elem) => {
+          return name.toLowerCase() === '' ?  elem : elem.fname.toLowerCase().includes(name)
+         }  ).map((elem) => {
             return (
                 <Card elem={elem} key={elem?.id} />
              
